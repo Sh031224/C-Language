ebe08b2132d8f90cc5ecd0c25ff8d03f55ebe3d6
Sh031224 <51149996+Sh031224@users.noreply.github.com>
2019-06-05 13:29:47 +0900

    C_Programming

---
+++ 2019-06-05.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+//2차원 배열
+
+void showarr2dstyle(int(*arr)[4], int column) {
+	for (int i = 0; i < column; i++) {
+		for (int j = 0; j < 4; j++)
+			printf("%d ", arr[i][j]);
+		printf("\n");
+	}
+	printf("\n");
+}
+
+int sum2darr(int arr[][4], int column) {
+	int sum = 0;
+	for (int i = 0; i < column; i++)
+		for (int j = 0; j < 4; j++)
+			sum += arr[i][j];
+	return sum;
+}
+
+int main(void)
+{
+	int arr1[2][4] = { 1,2,3,4,5,6,7,8 };
+	int arr2[3][4] = { 1,1,1,1,3,3,3,3,5,5,5,5 };
+
+	showarr2dstyle(arr1, sizeof(arr1) / sizeof(arr1[0]));
+	showarr2dstyle(arr2, sizeof(arr2) / sizeof(arr2[0]));
+
+	printf("arr1의 합: %d \n", sum2darr(arr1, sizeof(arr1) / sizeof(arr1[0])));
+	printf("arr2의 합: %d \n", sum2darr(arr2, sizeof(arr2) / sizeof(arr2[0])));
+	return 0;
+}
