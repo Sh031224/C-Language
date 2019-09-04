fdd35fc21d51f679ccf9ce8f41b1fe24e0fbcde4
Sh031224 <51149996+Sh031224@users.noreply.github.com>
2019-09-04 09:58:24 +0900

    C_Programming

---
+++ 피보나치 수열 n번째 수.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int fibo(int num) {
+	if (num == 0) return 0;
+	else if (num == 1) return 1;
+	else return fibo(num - 1) + fibo(num - 2);
+}
+
+
+int main() {
+	int input;
+	scanf("%d", &input);
+	printf("%d ", fibo(input - 1));
+}
